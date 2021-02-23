12fa7f11b89c914eea9aecbdf3ec6c3055e823f5
yason0319 <yason0319@gmail.com>
2021-02-23 12:08:24 +0900

    query return object

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
 const { ApolloServer, ApolloError } = require('apollo-server');
 
 const typeDefs = `
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
+
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
   }
 
   type Mutation {
-    postPhoto(name: String! description: String): Boolean!
+    postPhoto(name: String! description: String): Photo!
   }
 `
 
+let _id = 0
 let photos = [];
 
 const resolvers = {
   Query: {
-    totalPhotos: () => photos.length
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos
   },
 
   Mutation: {
     postPhoto(parent, args) {
-      photos.push(args)
-      return true
+      let newPhoto = {
+        id: _id++,
+        ...args
+      }
+      photos.push(newPhoto)
+      return newPhoto
     }
+  },
+
+  Photo: {
+    url: parent => `http://honyahonya/img/${parent.id}.jpg`
   }
 }
 
