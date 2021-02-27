9fa87668c0fd2257d1e3917baf912f9353c10732
yason0319 <yason0319@gmail.com>
2021-02-27 18:32:05 +0900

    add github auth

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const expressPlayground = require('graphql-playground-middleware-express').default;
 const { readFileSync } = require('fs');
 const { MongoClient } = require('mongodb');
+const resolvers = require('./resolvers');
 require('dotenv').config();
 
 const { GraphQLScalarType } = require('graphql');
@@ -10,112 +11,6 @@ const { GraphQLScalarType } = require('graphql');
 const typeDefs = readFileSync('./typeDefs.graphql', 'utf-8');
 
 const app = express();
-
-// let _id = 0
-
-// // sample data
-// const users = [
-//   { "githubLogin": "ichi", "name": "ichi san" },
-//   { "githubLogin": "ni", "name": "ni san" },
-//   { "githubLogin": "san", "name": "san san" }
-// ];
-
-// const photos = [
-//   {
-//     "id": "1",
-//     "name": "sample photo2",
-//     "url": "http://honyahonya/img/0.jpg",
-//     "description": "sample sample",
-//     "category": "PORTRAIT",
-
-//     "githubUser": "ichi",
-//     "created": "3-28-1977",
-//   },
-//   {
-//     "id": "2",
-//     "name": "sample photo2",
-//     "url": "http://honyahonya/img/0.jpg",
-//     "description": "sample sample",
-//     "category": "PORTRAIT",
-//     "githubUser": "ni",
-//     "created": "1-28-1977",
-//   },
-//   {
-//     "id": "3",
-//     "name": "sample photo2",
-//     "url": "http://honyahonya/img/0.jpg",
-//     "description": "sample sample",
-//     "category": "PORTRAIT",
-//     "githubUser": "ni",
-//     "created": "2018-04-15T19:09:57.204Z",
-//   }
-// ];
-
-// const tags = [
-//   { "photoID": "1", "userID": "ichi" },
-//   { "photoID": "2", "userID": "ichi" },
-//   { "photoID": "2", "userID": "ni" },
-//   { "photoID": "2", "userID": "san" },
-// ]
-
-// const resolvers = {
-//   Query: {
-//     totalPhotos: () => photos.length,
-//     allPhotos: () => photos
-//   },
-
-//   Mutation: {
-//     postPhoto(parent, args) {
-//       let newPhoto = {
-//         id: _id++,
-//         ...args.input,
-//         created: new Date(),
-//       }
-//       photos.push(newPhoto)
-//       return newPhoto
-//     },
-//   },
-
-//   Photo: {
-//     url: parent => `http://honyahonya/img/${parent.id}.jpg`,
-//     postedBy: parent => {
-//       return users.find(u => u.githubLogin === parent.githubUser)
-//     },
-//     taggedUsers: parent => {
-//       return tags
-//         .filter(tag => tag.photoID === parent.id)
-//         .map(tag => tag.userID)
-//         .map(userID => users.find(u => u.githubLogin === userID))
-//     }
-//   },
-//   User: {
-//     postedPhotos: parent => {
-//       return photos.filter(p => p.githubUser === parent.githubLogin)
-//     },
-//     inPhotos: parent => {
-//       return tags
-//         .filter(tag => tag.photoID === parent.id)
-//         .map(tag => tag.userID)
-//         .map(photoID => photos.find(p => p.id === photoID))
-//     }
-//   },
-//   DateTime: new GraphQLScalarType({
-//     name: 'DateTime',
-//     description: 'A valid date time value',
-//     parseValue: value => new Date(value),
-//     serialize: value => new Date(value).toISOString(),
-//     parseLiteral: ast => ast.value
-//   })
-// }
-
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers
-// })
-
-// server
-//   .listen()
-//   .then(({url}) => console.log(`graphql server running at ${url}`));
 
 async function start() {
   const app = express();
--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,3 +1,5 @@
+const { authorizeWithGithub } = require('../lib')
+
 module.exports = {
   postPhoto(parent, args) {
     let newPhoto = {
@@ -7,5 +9,42 @@ module.exports = {
     }
     photos.push(newPhoto)
     return newPhoto
+  },
+
+  async githubAuth(parent, { code }, { db }) {
+
+    console.log(code);
+
+    console.log(process.env.CLIENT_ID);
+    console.log(process.env.CLIENT_SECRET);
+
+    let {
+      message,
+      access_token,
+      avatar_url,
+      login,
+      name
+    } = await authorizeWithGithub({
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      code
+    })
+
+    if (message) {
+      throw new Error(message);
+    }
+
+    let latestUserInfo = {
+      name,
+      githubLogin: login,
+      githubToken: access_token,
+      avatar: avatar_url
+    }
+
+    const { ops:[user] } = await db
+      .collection('users')
+      .replaceOne({ githubLogin: login }, latestUserInfo, { upsert: true })
+
+    return { user, token: access_token }
   }
 }
